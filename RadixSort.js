8ed26282ca19d263be03740f7e95e0fac111fdd4
LAPTOP-465HP6VI\Victor <vduvanko1@gmail.com>
2019-03-21 18:29:47 -0400

    Cleaned up and simplified Radix Sort implementations. Documented the interfaces. Fixed a bug with repo link that Andy found. Changed names of the sorting functions to represent their function closer.

--- RadixSort.js
+++ RadixSort.js
@@ -1,6 +1,6 @@
-var HPCjs = HPCjs || {};
+var HpcAlgorithms = HpcAlgorithms || {};
 
-HPCjs.Algorithm = (function()
+HpcAlgorithms.Sorting = (function()
 {
     var internalState = "Message";
 
@@ -17,89 +17,96 @@ HPCjs.Algorithm = (function()
 		var digit = (a & bitMask) >>> shiftRightAmount; // extract the digit we are sorting based on
 		return digit;
 	}
-	
-	// Sorts an array of unsigned integers only
-	// Be careful as Javascript has only the notion of a number and not unsigned integer, signed integer, floating-point, double, decimal, etc.
-	var sortRadixLsdUnsigned = function(_input_array)
+
+	/**
+	 * Radix Sort (Least Significant Digit - LSD) of unsigned integer array with values up to 32-bits (e.g. 0, 1, 2, ... 2_000_000_000, ...)
+	 * This algorithm is not in-place - i.e. returns a sorted array
+	 * @param  {Array of numbers} inputArray Array of numbers, which must be unsigned integers of values within 32-bits
+	 * @return {Array of numbers} Sorted array of numbers
+	 */
+	var RadixSortLsdUInt32 = function(inputArray)
 	{
-		if (typeof _input_array.constructor === Array && typeof _input_array[0] === "number") throw new TypeError("Input argument must be an array of unsigned integers");
+		if (typeof inputArray.constructor === Array && typeof inputArray[0] === "number") throw new TypeError("Input argument must be an array of unsigned integers");
 		var numberOfBins = 256;
 		var Log2ofPowerOfTwoRadix = 8;
-		var _output_array = new Array(_input_array.length);
+		var outputArray = new Array(inputArray.length);
 		var count = new Array(numberOfBins);
-		var _output_array_has_result = false;
+		var outputArrayHasResult = false;
 
 		var bitMask = 255;
 		var shiftRightAmount = 0;
 
 		var startOfBin = new Array( numberOfBins );
-		var endOfBin   = new Array( numberOfBins );
-
+	
 		while( bitMask != 0 ) // end processing digits when all the mask bits have been processed and shifted out, leaving no bits set in the bitMask
 		{
 			for (var i = 0; i < numberOfBins; i++ )
 				count[ i ] = 0;
-			for (var _current = 0; _current < _input_array.length; _current++ ) // Scan the array and count the number of times each digit value appears - i.e. size of each bin
-				count[ extractDigit( _input_array[ _current ], bitMask, shiftRightAmount ) ]++;
+			for (var current = 0; current < inputArray.length; current++ ) // Scan the array and count the number of times each digit value appears - i.e. size of each bin
+				count[ extractDigit( inputArray[ current ], bitMask, shiftRightAmount ) ]++;
 	 
-			startOfBin[ 0 ] = endOfBin[ 0 ] = 0;
+			startOfBin[ 0 ] = 0;
 			for( var i = 1; i < numberOfBins; i++ )
-				startOfBin[ i ] = endOfBin[ i ] = startOfBin[ i - 1 ] + count[ i - 1 ];
-			for ( var _current = 0; _current < _input_array.length; _current++ )
-				_output_array[ endOfBin[ extractDigit( _input_array[ _current ], bitMask, shiftRightAmount ) ]++ ] = _input_array[ _current ];
+				startOfBin[ i ] = startOfBin[ i - 1 ] + count[ i - 1 ];
+			for ( var current = 0; current < inputArray.length; current++ )
+				outputArray[ startOfBin[ extractDigit( inputArray[ current ], bitMask, shiftRightAmount ) ]++ ] = inputArray[ current ];
 	 
 			bitMask <<= Log2ofPowerOfTwoRadix;
 			shiftRightAmount += Log2ofPowerOfTwoRadix;
-			_output_array_has_result = !_output_array_has_result;
+			outputArrayHasResult = !outputArrayHasResult;
 	 
-			var tmp = _input_array, _input_array = _output_array, _output_array = tmp; // swap input and output arrays
+			var tmp = inputArray, inputArray = outputArray, outputArray = tmp; // swap input and output arrays
 		}
-		if ( _output_array_has_result )
-			for ( var _current = 0; _current < _input_array.length; _current++ ) // copy from output array into the input array
-				_input_array[ _current ] = _output_array[ _current ];
+		if ( outputArrayHasResult )
+			for ( var current = 0; current < inputArray.length; current++ ) // copy from output array into the input array
+				inputArray[ current ] = outputArray[ current ];
 	 
-		return _input_array;
+		return inputArray;
 	}
 	
-	// Sorts an array of user define classes based on an unsigned integer keys only
-	// Be careful as Javascript has only the notion of a number and not unsigned integer, signed integer, floating-point, double, decimal, etc.
-	var sortRadixLsdUserUnsigned = function(_input_array, getKey)
+	/**
+	 * Radix Sort (Least Significant Digit - LSD) of user defined type/class array based on a key that is a 32-bit unsigned integer (e.g. 0, 1, 2, ... 2_000_000_000, ...)
+	 * This algorithm is not in-place - i.e. returns a sorted array. This algorithm is stable.
+	 * @param  {Array} inputArray user defined type/class  (UDT) array with each element containing a key that is a 32-bit unsigned integer
+	 * @param  {function} getKey function to extract and return a numeric key from the user defined type/class to sort on
+	 * @return {Array of numbers} Sorted array of a user defined type
+	 */
+	var RadixSortLsdUdtUInt32 = function(inputArray, getKey)
 	{
 		var numberOfBins = 256;
 		var Log2ofPowerOfTwoRadix = 8;
-		var _output_array = new Array(_input_array.length);
+		var OutputArray = new Array(inputArray.length);
 		var count = new Array(numberOfBins);
-		var _output_array_has_result = false;
+		var OutputArrayHasResult = false;
 		var bitMask = 255;
 		var shiftRightAmount = 0;
 		var startOfBin = new Array( numberOfBins );
-		var endOfBin   = new Array( numberOfBins );
 
 		while( bitMask != 0 )    // end processing digits when all the mask bits have been processed and shifted out, leaving no bits set in the bitMask
 		{
 			for (var i = 0; i < numberOfBins; i++ )
 				count[ i ] = 0;
-			for (var _current = 0; _current < _input_array.length; _current++ )    // Scan the array and count the number of times each digit value appears - i.e. size of each bin
-				count[ extractDigit( getKey(_input_array[ _current ]), bitMask, shiftRightAmount ) ]++;
-			startOfBin[ 0 ] = endOfBin[ 0 ] = 0;
+			for (var current = 0; current < inputArray.length; current++ )    // Scan the array and count the number of times each digit value appears - i.e. size of each bin
+				count[ extractDigit( getKey(inputArray[ current ]), bitMask, shiftRightAmount ) ]++;
+			startOfBin[ 0 ] = 0;
 			for( var i = 1; i < numberOfBins; i++ )
-				startOfBin[ i ] = endOfBin[ i ] = startOfBin[ i - 1 ] + count[ i - 1 ];
-			for ( var _current = 0; _current < _input_array.length; _current++ )
-				_output_array[ endOfBin[ extractDigit( getKey(_input_array[ _current ]), bitMask, shiftRightAmount ) ]++ ] = _input_array[ _current ];
+				startOfBin[ i ] = startOfBin[ i - 1 ] + count[ i - 1 ];
+			for ( var current = 0; current < inputArray.length; current++ )
+				OutputArray[ startOfBin[ extractDigit( getKey(inputArray[ current ]), bitMask, shiftRightAmount ) ]++ ] = inputArray[ current ];
 			bitMask <<= Log2ofPowerOfTwoRadix;
 			shiftRightAmount += Log2ofPowerOfTwoRadix;
-			_output_array_has_result = !_output_array_has_result;
-			var tmp = _input_array, _input_array = _output_array, _output_array = tmp;    // swap input and output arrays
+			OutputArrayHasResult = !OutputArrayHasResult;
+			var tmp = inputArray, inputArray = OutputArray, OutputArray = tmp;    // swap input and output arrays
 		}
-		if ( _output_array_has_result )
-			for ( var _current = 0; _current < _input_array.length; _current++ )    // copy from output array into the input array
-				_input_array[ _current ] = _output_array[ _current ];
-		return _input_array;
+		if ( OutputArrayHasResult )
+			for ( var current = 0; current < inputArray.length; current++ )    // copy from output array into the input array
+				inputArray[ current ] = OutputArray[ current ];
+		return inputArray;
 	}
 	
     return {
         //someProperty: 'prop value',
-        sortRadixLsd: sortRadixLsdUnsigned,
-		sortRadixLsdUser: sortRadixLsdUserUnsigned
+        RadixSortLsdUInt32: RadixSortLsdUInt32,
+		RadixSortLsdUdtUInt32: RadixSortLsdUdtUInt32
     };
 })();
