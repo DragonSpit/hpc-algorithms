f1884130f76efa3086aa64b858a458a4786492c5
LAPTOP-465HP6VI\Victor <vduvanko1@gmail.com>
2019-03-24 20:30:43 -0400

    Higher performance LSD Radix Sort is working, which performs only a single pass over the input array for the Count (1st phase) and then uses it in the permutation phase (2nd phase). I implemented the same optimization in C# earlier.

--- RadixSort.js
+++ RadixSort.js
@@ -1,3 +1,6 @@
+// TODO: It may be faster to do shift-mask instead of the current mask-shift, since the mask would then be the same (0xff).
+// TODO: Remove extract digit function and just inline the core code. I've seem this improve performance in C#. Not sure how agressively JavaScript inlines small functions.
+
 var HpcAlgorithms = HpcAlgorithms || {};
 
 HpcAlgorithms.Sorting = (function()
@@ -63,7 +66,79 @@ HpcAlgorithms.Sorting = (function()
 	 
 		return inputArray;
 	}
+
+	var HistogramByteComponents = function(inArray, l, r)
+	{
+	  var numberOfDigits = 4;
+	  var numberOfBins = 256;
 	
+	  var count = new Array(numberOfDigits);
+	  for (var d = 0; d < numberOfDigits; d++)
+	  {
+		count[d] = new Array(numberOfBins);
+		for (var b = 0; b < numberOfBins; b++)
+		  count[d][b] = 0;
+	  }
+	  
+	  for (var current = l; current <= r; current++)    // Scan the array and count the number of times each digit value appears - i.e. size of each bin
+	  {
+		var value = inArray[current];
+		count[0][ value &       0xff       ]++;
+		count[1][(value &     0xff00) >>  8]++;
+		count[2][(value &   0xff0000) >> 16]++;
+		count[3][(value & 0xff000000) >> 24]++;
+	  }
+	  return count;
+	}
+	
+	/**
+	 * Radix Sort (Least Significant Digit - LSD) of unsigned integer array with values up to 32-bits (e.g. 0, 1, 2, ... 2_000_000_000, ...)
+	 * This algorithm is not in-place - i.e. returns a sorted array
+	 * @param  {Array of numbers} inputArray Array of numbers, which must be unsigned integers of values within 32-bits
+	 * @return {Array of numbers} Sorted array of numbers
+	 */
+	var RadixSortLsdUInt32_new = function(inputArray)
+	{
+	  if (typeof inputArray.constructor === Array && typeof inputArray[0] === "number") throw new TypeError("Input argument must be an array of unsigned integers");
+	  var numberOfBins = 256;
+	  var numberOfDigits = 4;
+	  var Log2ofPowerOfTwoRadix = 8;
+	  var outputArrayHasResult = false;
+	  var bitMask = 255;
+	  var shiftRightAmount = 0;
+	  var outputArray = new Array(inputArray.length);
+	
+	  var count = HistogramByteComponents(inputArray, 0, inputArray.length - 1);
+	
+	  var startOfBin = new Array(numberOfDigits);
+	  var d = 0;
+	  for (d = 0; d < numberOfDigits; d++)
+	  {
+		startOfBin[d] = new Array(numberOfBins);
+		startOfBin[d][0] = 0;
+		for (var b = 1; b < numberOfBins; b++ )
+		  startOfBin[d][b] = startOfBin[d][b - 1] + count[d][b - 1];
+	  }
+	
+	  d = 0;
+	  while( bitMask != 0 ) // end processing digits when all the mask bits have been processed and shifted out, leaving no bits set in the bitMask
+	  {
+		//console.log('d: ' + d);
+		var startOfBinLoc = startOfBin[d];
+		for ( var current = 0; current < inputArray.length; current++ )
+		  outputArray[ startOfBinLoc[ extractDigit( inputArray[ current ], bitMask, shiftRightAmount ) ]++ ] = inputArray[ current ];
+	 
+		bitMask <<= Log2ofPowerOfTwoRadix;
+		shiftRightAmount += Log2ofPowerOfTwoRadix;
+		outputArrayHasResult = !outputArrayHasResult;
+		d++;
+	 
+		var tmp = inputArray, inputArray = outputArray, outputArray = tmp; // swap input and output arrays
+	  }
+	 
+	  return outputArrayHasResult ? outputArray : inputArray;;
+	}
+		
 	/**
 	 * Radix Sort (Least Significant Digit - LSD) of user defined type/class array based on a key that is a 32-bit unsigned integer (e.g. 0, 1, 2, ... 2_000_000_000, ...)
 	 * This algorithm is not in-place - i.e. returns a sorted array. This algorithm is stable.
@@ -107,6 +182,7 @@ HpcAlgorithms.Sorting = (function()
     return {
         //someProperty: 'prop value',
         RadixSortLsdUInt32: RadixSortLsdUInt32,
+        RadixSortLsdUInt32_new: RadixSortLsdUInt32_new,
 		RadixSortLsdUdtUInt32: RadixSortLsdUdtUInt32
     };
 })();
